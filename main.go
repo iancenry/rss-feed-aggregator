aa0e5cd4c3822a40bf59d6aa5cf34d9a310bdd4a
Ian Cenry <ian.c@nathandigital.com>
2024-08-31 22:13:59 +0300

    add driver

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/iancenry/rss-feed-aggregator/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+// holds a connection to a DB
+type apiConfig struct {
+	DB *database.Queries
+} 
 
 func main(){
 	godotenv.Load()
 	port := os.Getenv("PORT") 
+	if port == "" {
+		log.Fatal("PORT isn't found in the environment")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL isn't found in the environment")
+	}
+
+	conn, er :=  sql.Open("postgres", dbURL)
+	if er != nil {
+		log.Fatal("Can't connect to database")
+	}
+
 
 	router := chi.NewRouter()
 
